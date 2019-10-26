6e15e550703b83c2c7022e3234866558905c478a
Alex Rivera <ajrivera721@gmail.com>
2019-10-26 10:35:48 -0700

    Added drawer navigation

--- group-tutor/screens/MainStudent.js
+++ group-tutor/screens/MainStudent.js
@@ -1,10 +1,53 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import { createDrawerNavigator, createAppContainer } from 'react-navigation';
 
-class MainStudent extends Component {
+class QueueScreen extends Component {
+    static navigationOptions = {
+      drawerLabel: 'Queue',
+    };
+  
     render() {
-        return(<View></View>);
+      return (
+        <View></View>
+      );
     }
-}
+  }
+  
+  class TutorScreen extends Component {
+    static navigationOptions = {
+      drawerLabel: 'Tutor',
+    };
+  
+    render() {
+      return (
+        <View></View>
+      );
+    }
+  }
 
-export default MainStudent;
+  class HistoryScreen extends Component {
+    static navigationOptions = {
+      drawerLabel: 'History',
+    };
+  
+    render() {
+      return (
+        <View></View>
+      );
+    }
+  }
+  
+  const DrawerNavigator = createDrawerNavigator({
+    Queue: {
+      screen: QueueScreen,
+    },
+    Tutors: {
+      screen: TutorScreen,
+    },
+    History: {
+        screen: HistoryScreen,
+    },
+  });
+  
+  export default createAppContainer(DrawerNavigator);
