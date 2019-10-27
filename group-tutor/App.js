4f1ac65a1684a2419b4fe87c7a2fe8cc3cb76e97
Johnathan Huynh <>
2019-10-27 01:33:28 -0700

    Importing fonts

--- group-tutor/App.js
+++ group-tutor/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { createAppContainer } from 'react-navigation';
+import { View, Text } from 'react-native';
 import { createSwitchNavigator } from 'react-navigation';
 import SplashScreen from './screens/SplashScreen';
 import LoginScreen from './screens/LoginScreen';
@@ -22,8 +23,17 @@ const RootSwitch = createSwitchNavigator(
 
 const AppContainer = createAppContainer(RootSwitch);
 
-export default class App extends Component {
-
+export default class App extends React.Component {
+    state = {
+      fontLoaded: false,
+    }
+    async componentWillMount() {
+      await Expo.Font.loadAsync({
+        'SFPro': require('./assets/fonts/SFProText-Light.otf'),
+      });
+      this.setState({ fontLoaded: true });
+    }
+  
   render() {
     return <AppContainer />;
   }
